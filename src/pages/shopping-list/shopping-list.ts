5d81351ebce6416ee6cb834c9da4f66f2c9633d9
louca75 <louca.mendy@gmail.com>
2018-02-16 16:37:58 +0100

    création du model ingrédients et service ingrédients

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { NgForm } from '@angular/forms/src/directives/ng_form';
 
-
 @IonicPage()
 @Component({
   selector: 'page-shopping-list',
---
+++ src/services/shopping-list.service.ts
@@ -0,0 +1,28 @@
+import { Ingredient } from "../models/ingredient";
+
+
+
+
+export class ShoppingListService {
+
+    private listeIngredients: Ingredient [] = [];
+
+    ajouterIngredient(name: string, nombre:number) {
+        this.listeIngredients.push(new Ingredient(name, nombre));
+    }
+
+  /*   ES 6, divise en un élément chaque Elements  */
+    ajouterIngredients(ingredients: Ingredient[]){
+        this.listeIngredients.push(...ingredients);
+    }
+
+    supprimerIngredient(index: number) {
+        this.listeIngredients.splice(index,1);
+    }
+    
+/*     on envoi une copie */
+    getIngredients() {
+        return this.listeIngredients.slice();
+    }
+
+}
