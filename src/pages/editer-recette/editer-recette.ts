328da432149424d6bd5eef1af41c8934f77aa965
louca75 <louca.mendy@gmail.com>
2018-03-20 13:09:54 +0100

    ajout toasts, alerte Controller

--- src/pages/editer-recette/editer-recette.ts
+++ src/pages/editer-recette/editer-recette.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { ActionSheetController } from 'ionic-angular/components/action-sheet/action-sheet-controller';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
+import { ToastController } from 'ionic-angular/components/toast/toast-controller';
+
 
 
 @IonicPage()
@@ -20,8 +24,52 @@ export class EditerRecettePage implements OnInit{
     this.initializeForm();
   }
 
+  constructor(
+    private navCtrl: NavController,
+    private navParams: NavParams, 
+    private actionSheetController: ActionSheetController, 
+    private alertCtrl: AlertController,
+    private toastCtrl: ToastController)
+     {
+  }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  private creerNouvelIngredientAlerte() {
+    return this.alertCtrl.create({
+      title: 'Ajouter Ingredient',
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'nom'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Annuler',
+          role: 'cancel'
+        },
+        {
+          text: 'Ajouter',
+          handler: data => {
+            if (data.name.trim() == '' || null) {
+              const toast = this.toastCtrl.create({
+                message: 'Entrez une donnée valide',
+                duration: 2000,
+                position: 'bottom'
+              })
+              toast.present();
+            }
+            (<FormArray>this.recipeForm.get('ingredients'))
+            .push(new FormControl(data.name, Validators.required));
+            const toast = this.toastCtrl.create({
+              message: 'ingrédient ajouté',
+              duration: 2000,
+              position: 'top'
+            })
+            toast.present();
+          }
+        }
+      ]
+    });
   }
 
   /* REACTIVE FORM --- ANGULAR 2 */
@@ -29,11 +77,51 @@ export class EditerRecettePage implements OnInit{
     this.recipeForm = new FormGroup( {
       'titre' : new FormControl(null, Validators.required),
       'description' : new FormControl(null, Validators.required),
-      'difficulté': new FormControl('Moyenne',Validators.required)
+      'difficulté': new FormControl('Moyenne',Validators.required),
+      'ingredients': new FormArray([])
     });
   } 
   
   onSubmit() {
     console.log(this.recipeForm);
+  }
+
+  /* An Action Sheet is a dialog that lets the user choose from a set of options */
+  gererIngredients() {
+   const actionSheet = this.actionSheetController.create({
+      title: 'Que voulez vous faire ?',
+      buttons: [
+        {
+          text: 'Ajouter un ingrédient',
+          handler: () => {
+            this.creerNouvelIngredientAlerte().present();
+          }
+        },
+        {
+          text: 'Supprimer tous les ingrédients',
+          role: 'detruire',
+          handler: () => {
+            const fArray: FormArray = <FormArray>this.recipeForm.get('ingredients');
+            const len = fArray.length;
+            if (len>0) {
+              for (let i =len -1; i>=0; i-1) {
+                fArray.removeAt(i);
+              }
+              const toast = this.toastCtrl.create({
+                message: 'Tous les ingrédients ont été supprimés',
+                duration: 2000,
+                position: 'bottom'
+              })
+              toast.present();
+            }
+          }
+        },
+        {
+          text: 'Annuler',
+          role: 'cancel'
+        }
+        ]
+    });
+    actionSheet.present();
   }
 }
